907a65637e2c274a9144560860b176e8de3ebaf4
Gabriel <gabrieloliveiravaladares@gmail.com>
2025-07-30 17:50:02 -0300

    feat: styling with responsiveness

--- src/Frontend/scripts/contact.js
+++ src/Frontend/scripts/contact.js
@@ -1,8 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", sendEmail);
+});
+
+
 function sendEmail(event) {
   event.preventDefault();
 
   const submitButton = document.getElementById("submit");
   const form = document.getElementById("contact-form");
+
+  function showTooltipMessage(message, isSuccess = true) {
+  const popup = document.getElementById("popUpEmailSend");
+  popup.textContent = message;
+  popup.classList.add("show");
+
+  setTimeout(() => {
+    popup.classList.remove("show");
+  }, 3000);
+  }
+
+  const language = localStorage.getItem("language") || "english";
+  let sending = "";
+  let success = "";
+  let emailError = "";
+
+  if (language === "portuguese") {
+      sending = "Enviando";
+      success = "Email enviado com sucesso!";
+      emailError = "Erro ao enviar o email. Por favor, tente novamente!";
+  } else {
+      sending = "Sending";
+      success = "Email sent successfully!";
+      emailError = "Error sending email. Please try again.";
+  }
+
 
   if (!form.checkValidity()) {
     form.reportValidity();
@@ -11,8 +43,7 @@ function sendEmail(event) {
 
   submitButton.disabled = true;
   const originalText = submitButton.innerText;
-  submitButton.innerText = "Sending...";
-
+  submitButton.innerText = sending;
   const templateParams = {
     firstName: document.querySelector("#firstName").value.trim(),
     lastName: document.querySelector("#lastName").value.trim(),
@@ -20,22 +51,18 @@ function sendEmail(event) {
     subject: document.querySelector("#subject").value.trim(),
     message: document.querySelector("#message").value.trim(),
   };
-
   emailjs
-    .send("service_urnvjsn", "template_kpw0d6m", templateParams)
+    .send("service_y2pfg6q", "template_r3eyioq", templateParams)
     .then(() => {
-      alert("Email sent successfully!");
+      showTooltipMessage(success);
       form.reset();
     })
     .catch((error) => {
       console.error("Error sending email:", error);
-      alert("Failed to send email. Please try again.");
+     showTooltipMessage(emailError, false);
     })
     .finally(() => {
       submitButton.disabled = false;
       submitButton.innerText = originalText;
     });
 }
-
-const form = document.getElementById("contact-form");
-form.addEventListener("submit", sendEmail);
